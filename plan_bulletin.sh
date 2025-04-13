650380440ab7bbb2350e6cc47b5bc642604ef66e
KayuProg <KayuProg@gmail.com>
2025-04-13 20:00:29 +0900

    fix at win

---
+++ plan_bulletin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#chmod +x ./main.py
+cd /home/kayu/Desktop/plan_bulletin_raspi
+source .venv/bin/activate
+
+#sleep 4
+LIBGL_ALWAYS_SOFTWARE=1 python3 ./main.py
+
+#python ./main.py
+
+# Python ƒXƒNƒŠƒvƒg‚ÌƒpƒX‚ðŽw’è
+#PYTHON_SCRIPT="/home/kayu/Desktop/plan/main.py"
+
+#sleep 3
+
+# Python ƒCƒ“ƒ^ƒvƒŠƒ^‚ÌƒpƒX‚ðŽw’è
+#PYTHON_EXEC="/usr/bin/python3"
+
+# Python ƒXƒNƒŠƒvƒg‚ðŽÀs
+#$PYTHON_EXEC $PYTHON_SCRIPT
